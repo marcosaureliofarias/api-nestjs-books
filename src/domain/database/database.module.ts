496e0699cb0edb33f0d021bca2f7acb5dfc5a25f
marcos.farias <marcos.farias@procer.com.br>
2024-09-01 21:48:14 -0300

    feat: nova configuracao

--- src/domain/database/database.module.ts
+++ src/domain/database/database.module.ts
@@ -6,13 +6,16 @@ import { BookEntity } from '../book/entity/book.entity';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: 'dpg-cragbsbtq21c73c00ed0-a.oregon-postgres.render.com',
   port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'bookrental',
+  username: 'marcos_41on_user',
+  password: '5v5hJXSoGYOlxUDbmeGi3U514FC5geg4',
+  database: 'marcos_41on',
   entities: [CreateUserEntity, BookEntity],
   synchronize: true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
 
 @Module({
